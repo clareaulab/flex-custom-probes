96e71cc5a64305445ff389fd90ce4515c7a6a39e
caleblareau <caleb.lareau@gmail.com>
2025-01-01 14:21:35 -0500

    CAR custom setup

--- design_runs.py
+++ design_runs.py
@@ -6,24 +6,25 @@ from utils import transcriptome, export_probes_to_xlsx
 from probe_designer import ProbeDesigner
 
 
-def design_hhv6_probes(human_background=True):
+def design_car_probes(human_background=False):
     transcriptome_dict = dict()
-    transcriptome_fasta = "HHV6B_AF157706.fasta"
+    transcriptome_fasta = "CAR/WL_CARconstruct.fasta"
     for record in SeqIO.parse(transcriptome_fasta, "fasta"):
         transcriptome_dict[record.description] = str(record.seq)
-    hhv6_transcriptome = transcriptome_dict.copy()
+    design_me = transcriptome_dict.copy()
 
     if human_background:
         ensembl_genome = EnsemblRelease(111)
         ensembl_genome.download(overwrite=False)
         ensembl_genome.index(overwrite=False)
         transcriptome_dict = transcriptome_dict | transcriptome(ensembl_genome)
-
-    probe_design = ProbeDesigner(working_dir="hhv6_probe_design", reference_database=transcriptome_dict)
+	print("here1")
+	
+    probe_design = ProbeDesigner(working_dir="CAR", reference_database=transcriptome_dict)
     n_probes = 3
     results = []
     missing_probes = []
-    for name, sequence in hhv6_transcriptome.items():
+    for name, sequence in design_me.items():
         probes = probe_design.create_probes(name, sequence, n_probes=n_probes)
         if probes is None or len(probes) < 1:
             missing_probes.append(name)
@@ -36,15 +37,15 @@ def design_hhv6_probes(human_background=True):
     df = export_probes_to_xlsx(results, f"HHV6_probes_with_human_background_info.xlsx", n_barcodes=4)
     # Add truseq barcodes
     df['lhs_TruSeq_probe'] = "GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT" + df['lhs_seq']
-    df.to_excel(f"HHV6_probes_with_human_background_info.xlsx", index=False)
-    df = export_probes_to_xlsx(results, f"HHV6_probes_with_human_background_1barcode_info.xlsx", n_barcodes=1)
+    df.to_excel(f"CAR/WL_CAR_probes_with_human_background_info.xlsx", index=False)
+    df = export_probes_to_xlsx(results, f"CAR/WL_CARprobes_with_human_background_1barcode_info.xlsx", n_barcodes=1)
     # Add truseq barcodes
     df['lhs_TruSeq_probe'] = "GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT" + df['lhs_seq']
-    df.to_excel(f"HHV6_probes_with_human_background_1barcode_info.xlsx", index=False)
+    df.to_excel(f"CAR/WL_CAR_probes_with_human_background_1barcode_info.xlsx", index=False)
     print(f"Missing {len([r for r in results if r is None])} probes.")
 
     return pd.DataFrame(results)
 
 
 if __name__ == "__main__":
-    design_hhv6_probes(human_background=True)
+    design_car_probes(human_background=False)
